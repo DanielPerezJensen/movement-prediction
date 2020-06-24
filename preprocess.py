eaec22acc77fe5ebb29e149ae0e860ea6236896f
Daniel Perez Jensen <danielperezjensen@gmail.com>
2020-06-24 11:44:48 +0200

    Added sliding window preprocessing
    Raw data is preprocessed by sliding over the data and converting it
    to whatever n required.

--- preprocess.py
+++ preprocess.py
@@ -13,20 +13,23 @@ with open("data/data.txt", "r") as f:
         except SyntaxError:
             pass
 
-# Convert list of tuples to dataframe and save it as a csv
-path_data = []
-for path in data:
-    if len(path) > 5:
-        for i in range(len(path[:-5])):
-            x0, y0 = path[i]
-            x1, y1 = path[i + 1]
-            x2, y2 = path[i + 2]
-            x5, y5 = path[i + 5]
-            path_datapoint = [x0, y0, x1, y1, x2, y2, x5, y5]
-            path_data.append(path_datapoint)
+ns = [1, 2, 3, 4, 5]
 
+for n in ns:
+    print(n)
+    # Convert list of tuples to dataframe and save it as a csv
+    path_data = []
+    for path in data:
+        if len(path) > 2 + n:
+            for i in range(len(path[:-2 - n])):
+                x0, y0 = path[i]
+                x1, y1 = path[i + 1]
+                x2, y2 = path[i + 2]
+                x5, y5 = path[i + 2 + n]
+                path_datapoint = [x0, y0, x1, y1, x2, y2, x5, y5]
+                path_data.append(path_datapoint)
 
-path_df = pd.DataFrame(path_data,
-                       columns=["x0", "y0", "x1", "y1", "x2", "y2",
-                                "x5", "y5"])
-path_df.to_csv("data/preprocessed_data.csv", sep="\t")
+    path_df = pd.DataFrame(path_data,
+                           columns=["x0", "y0", "x1", "y1", "x2", "y2",
+                                    "x3", "y3"])
+    path_df.to_csv(f"data/preprocessed_data-n={n}.csv", sep="\t")
